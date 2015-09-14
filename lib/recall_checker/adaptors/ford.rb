ad34932304af0a49073feea601d474fc4877d946
ryedeer <bisyarin@mail.ru>
2015-09-14 19:55:21 +0300

    Small changes in Ford class

--- lib/recall_checker/adaptors/ford.rb
+++ lib/recall_checker/adaptors/ford.rb
@@ -1,7 +1,7 @@
 module RecallChecker
   module Adaptors
     class Ford < Base
-    	make 'ford'
+      make 'ford'
       # base_uri 'http://owner.ford.com/sharedServices/decodevin.do'
       base_uri 'http://owner.ford.com/sharedServices/recalls/query.do'
 
@@ -10,20 +10,20 @@ module RecallChecker
       end
 
       def add_items_to_recalls key
-      	if parsed_response.has_key?(key)
-      		parsed_response[key].each do |k,v| 
-      			@recalls << v["description_eng"] if v.is_a?(Hash) && v.has_key?("description_eng")
-      		end
-      	end
+        if parsed.response.is_a?(Hash) && parsed_response.has_key?(key)
+          parsed_response[key].each do |k,v| 
+            @recalls << v["description_eng"] if v.is_a?(Hash) && v.has_key?("description_eng")
+          end
+        end
       end
 
       def recalls
-      	if @recalls.nil?
-	    		@recalls = []
-	    		add_items_to_recalls "recalls"
-	    		add_items_to_recalls "fsa_items"
-	      	return @recalls
-      	end
+        if @recalls.nil?
+          @recalls = []
+          add_items_to_recalls "recalls"
+          add_items_to_recalls "fsa_items"
+        end
+        return @recalls
       end
 
     end
